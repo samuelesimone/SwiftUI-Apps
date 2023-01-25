bb793b942741a93a2fdc437a7956533924403f75
Samuele Simone <samuelesimone01@gmail.com>
2023-01-25 19:13:28 +0100

    Configuration Onboarding Fruits App

---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Samuele Simone on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
---
+++ Fructus/Fructus/App/OnboardingView.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Samuele Simone on 25/01/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    var body: some View {
+        TabView{
+            ForEach(0..<5){
+                item in
+                FruitCardView()
+            } //: LOOP
+            
+        }//: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical,10)
+       
+        
+    }
+}
+
+//MARK: - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
