8a2da4a983ccf9bcd59870246f095cce6cb53d39
Samuele Simone <samuelesimone01@gmail.com>
2023-02-02 22:54:13 +0100

    Configuration persistent db on Devote app

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Samuele Simone on 02/02/23.
+//
+
+import SwiftUI
+
+//MARK: - FORMATTER
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+//MARK: - UX
